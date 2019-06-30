c2b88ca4e92bededbc37c94e5d5c5ffa935b03e0
Mano <mano.net@gmail.com>
2019-06-30 10:05:48 -0600

    push
    push

--- DeleteAllKeepCSV.sh
+++ DeleteAllKeepCSV.sh
@@ -28,9 +28,29 @@ Delete_empty_folders() {
             echo "$i"
             Delete_empty_folders "$i"
             rmdir "$i" 
-            #find . -depth -exec rmdir {} \;  
-            #rm -rf `find . -type f -regex '.*/\.+.+'`
+            cd DataProducts
+            #ls
+            #Go to the folder and delete the data
+            find . -depth -exec rmdir {} \; 
+            rm -R ./.* 
             #echo "deleting $i"
+        fi
+    done
+}
+
+
+List_all_files() {
+    for i in "$1"/*;do
+        if [ -d "$i" ];then
+
+            for entry in `ls $i`; do
+                #if [ -f "$entry" ]; then
+                    echo $entry
+                #fi
+            done
+
+            #echo "Entering Directory: $i"
+            List_all_files "$i"
         fi
     done
 }
@@ -38,7 +58,9 @@ Delete_empty_folders() {
 clear 
 
 #Delete_files_except_CSV "test1"
-chmod -R 777 "DataProducts_201904"
+chmod -R 777 "DataProducts"
 
-Delete_files_except_CSV "DataProducts_201904"
-Delete_empty_folders "DataProducts_201904"
+#Delete_files_except_CSV "DataProducts"
+#Delete_empty_folders "DataProducts"
+
+List_all_files "DataProducts"
